11d8e457a5bc3f16fe7ca645d6dc125547586657
isepsd <daniqwert4@gmail.com>
2025-11-11 16:26:09 +0700

    updateeeeeeeeee

--- src/pages/spectrum/statusrealtime/telemetering.vue
+++ src/pages/spectrum/statusrealtime/telemetering.vue
@@ -83,33 +83,11 @@ const handleFilterChange = (newFilterValues: any) => {
 }
 </script>
 <template>
- <!-- Header -->
-  <VCard class="header-card mb-6 pa-4">
-    <VRow class="align-center">
-      <VCol cols="12">
+  <VCol cols="12">
     <AppCardCode title="Filter" :code="{ ts: '', js: '' }">
       <FillterFasopRealTimeTelemetering :onFilterChange="handleFilterChange" />
     </AppCardCode>
   </VCol>
-      <!-- Kolom kiri: ikon, judul, deskripsi, tombol -->
-      <VCol cols="12" md="8" class="d-flex align-start">
-        <VAvatar
-          size="48"
-          color="primary"
-          class="mr-4 elevation-2"
-        >
-          <!-- <VIcon icon="tabler-bolt" size="28" /> -->
-        </VAvatar>
-
-        <div class="header-info">
-          <h2 class="header-title">Telemetering</h2>
-          <p class="header-subtitle">
-          </p>
-         
-        </div>
-      </VCol>
-    </VRow>
-  </VCard>
 
 
     <!-- Table -->
